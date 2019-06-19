d43a464792734c9a8cfecb283a51154d066174c3
david.geirola <david.geirola.external@ynap.com>
2019-06-19 14:38:14 +0200

    Add XmlTraverseSyntax Tests.

---
+++ src/test/scala/com/dg/advxml/traverse/XmlTraverseSyntaxTest.scala
@@ -0,0 +1,121 @@
+package com.dg.advxml.traverse
+
+import org.scalatest.FeatureSpec
+
+/**
+  * Adxml
+  * Created by geirolad on 19/06/2019.
+  *
+  * @author geirolad
+  */
+class XmlTraverseSyntaxTest extends FeatureSpec {
+
+  import XmlTraverseSyntax._
+
+  feature("XmlTraverseSyntaxTest: Read Attributes") {
+    scenario("Read optional attribute") {
+      val xml =
+        <Employers>
+          <Employee Name="David"/>
+        </Employers>
+
+      val name = xml \ "Employee" \@? "Name"
+      val age = xml \ "Employee" \@? "Age"
+
+      assert(name.isDefined)
+      assert(name.get == "David")
+      assert(age.isEmpty)
+    }
+
+    scenario("Read required attribute") {
+      val xml =
+        <Employers>
+          <Employee Name="David"/>
+        </Employers>
+
+      val name = xml \ "Employee" \@! "Name"
+      val age = xml \ "Employee" \@! "Age"
+
+      assert(name.isSuccess)
+      assert(name.get == "David")
+      assert(age.isFailure)
+    }
+  }
+
+  feature("XmlTraverseSyntaxTest: Read Immediate Nodes") {
+    scenario("Read optional immediate nodes") {
+      val xml =
+        <Employers>
+          <Employee Name="David">
+            <Cars>
+              <Car Model="Fiat"/>
+            </Cars>
+          </Employee>
+        </Employers>
+
+      val cars = xml \ "Employee" \? "Cars"
+      val works = xml \ "Employee" \? "Works"
+
+      assert(cars.isDefined)
+      assert(cars.get.length == 1)
+      assert(works.isEmpty)
+    }
+
+    scenario("Read required immediate nodes") {
+      val xml =
+        <Employers>
+          <Employee Name="David">
+            <Cars>
+              <Car Model="Fiat"/>
+            </Cars>
+          </Employee>
+        </Employers>
+
+      val cars = xml \ "Employee" \! "Cars"
+      val works = xml \ "Employee" \! "Works"
+
+      assert(cars.isSuccess)
+      assert(cars.get.length == 1)
+      assert(works.isFailure)
+    }
+  }
+
+
+  feature("XmlTraverseSyntaxTest: Read nested Nodes") {
+    scenario("Read optional nested nodes") {
+      val xml =
+        <Employers>
+          <Employee Name="David">
+            <Cars>
+              <Car Model="Fiat"/>
+            </Cars>
+          </Employee>
+        </Employers>
+
+      val cars = xml \\? "Cars"
+      val works = xml \\? "Works"
+
+      assert(cars.isDefined)
+      assert(cars.get.length == 1)
+      assert(works.isEmpty)
+    }
+
+    scenario("Read required nested nodes") {
+      val xml =
+        <Employers>
+          <Employee Name="David">
+            <Cars>
+              <Car Model="Fiat"/>
+            </Cars>
+          </Employee>
+        </Employers>
+
+      val cars = xml \\! "Cars"
+      val works = xml \\! "Works"
+
+      assert(cars.isSuccess)
+      assert(cars.get.length == 1)
+      assert(works.isFailure)
+    }
+  }
+}
