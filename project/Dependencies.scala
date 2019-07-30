e4250cd50f2c21e250dd33d1981f37e5523a4b71
david.geirola <david.geirola.external@ynap.com>
2019-07-30 16:38:02 +0200

    Reorganize Sbt.

---
+++ project/Dependencies.scala
@@ -0,0 +1,22 @@
+import sbt.{CrossVersion, _}
+
+/**
+  * Advxml
+  * Created by geirolad on 30/07/2019.
+  *
+  * @author geirolad
+  */
+object Dependencies {
+
+  lazy val all: Seq[ModuleID] = Seq(
+    //SCALA
+    "org.typelevel" %% "cats-core" % "2.0.0-M4" cross CrossVersion.binary,
+    "org.scalactic" %% "scalactic" % "3.0.8" cross CrossVersion.binary,
+    //XML
+    "org.scala-lang.modules" %% "scala-xml" % "1.2.0" cross CrossVersion.binary,
+    //TEST
+    "org.scalatest" %% "scalatest" % "3.0.8" % Test cross CrossVersion.binary,
+    "org.scalatest" %% "scalatest" % "3.0.8" % Test cross CrossVersion.binary,
+    "org.scalacheck" %% "scalacheck" % "1.14.0" % Test cross CrossVersion.binary
+  )
+}
--- src/main/scala/com/github/geirolz/advxml/transform/XmlTransformer.scala
+++ src/main/scala/com/github/geirolz/advxml/transform/XmlTransformer.scala
@@ -7,16 +7,16 @@ import scala.xml.transform.RuleTransformer
 
 object XmlTransformer {
 
+  def transform[F[_]: MonadEx](root: NodeSeq, modifier: XmlModifier): F[NodeSeq] = modifier match {
+    case m: ComposableXmlModifier => XmlTransformer.transform(current(m))(root)
+    case m: FinalXmlModifier      => XmlTransformer.transform(current(m))(root)
+  }
+
   def current(modifier: FinalXmlModifier): FinalXmlRule =
     PartialXmlRule(identity) withModifier modifier
 
   def current(modifier: ComposableXmlModifier): ComposableXmlRule =
     PartialXmlRule(identity) withModifier modifier
-
-  def transform[F[_]: MonadEx](root: NodeSeq, modifier: XmlModifier): F[NodeSeq] = modifier match {
-    case m: ComposableXmlModifier => XmlTransformer.transform(current(m))(root)
-    case m: FinalXmlModifier      => XmlTransformer.transform(current(m))(root)
-  }
 
   def transform[F[_]: MonadEx](rule: XmlRule, rules: XmlRule*)(root: NodeSeq): F[NodeSeq] = {
 
