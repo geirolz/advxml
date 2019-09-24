217dafee310dacea6414fd0c35f930ad3eb3fd56
geirolz <david.geirola@gmail.com>
2019-09-24 14:07:17 +0200

    Add documentation.

--- src/main/scala/com/github/geirolz/advxml/convert/XmlConverter.scala
+++ src/main/scala/com/github/geirolz/advxml/convert/XmlConverter.scala
@@ -7,21 +7,54 @@ import com.github.geirolz.advxml.convert.XmlConverter.{ModelToXml, XmlToModel}
 import scala.xml.NodeSeq
 
 object XmlConverter extends ValidatedConverterOps {
+
+  /**
+    *
+    * @tparam A
+    * @tparam B
+    */
   type ModelToXml[-A, +B <: NodeSeq] = ValidatedConverter[A, B]
+
+  /**
+    *
+    * @tparam A
+    * @tparam B
+    */
   type XmlToModel[-A <: NodeSeq, +B] = ValidatedConverter[A, B]
 
+  /**
+    * Syntactic sugar to convert a [[Xml]] instance into [[Obj]] using an implicit [[XmlToModel]] instance.
+    * See [[XmlToModel]] for further information.
+    *
+    * @tparam Obj Output model type
+    * @return [[ValidatedRes]] instance that, if on success case contains [[Obj]] instance.
+    */
   def asXml[Obj: ModelToXml[?, Xml], Xml <: NodeSeq](model: Obj): ValidatedRes[Xml] = XmlConverter(model)
 
+  /**
+    * Syntactic sugar to convert a [[Obj]] instance into [[Xml]] using an implicit [[ModelToXml]] instance.
+    *
+    * @tparam Xml Output model type
+    * @return [[ValidatedRes]] instance that, if on success case contains [[Xml]] instance.
+    */
   def asModel[Xml <: NodeSeq: XmlToModel[?, Obj], Obj](xml: Xml): ValidatedRes[Obj] = XmlConverter(xml)
 }
 
 private[advxml] trait XmlConverterSyntax {
 
   implicit class XmlConverterOps[Xml <: NodeSeq](xml: Xml) {
+
+    /**
+      * @see [[XmlConverter.asModel()]]
+      */
     def as[Obj: XmlToModel[Xml, ?]]: ValidatedRes[Obj] = XmlConverter(xml)
   }
 
   implicit class ConverterAnyOps[Obj](model: Obj) {
+
+    /**
+      * @see [[XmlConverter.asXml()]]
+      */
     def asXml[Xml <: NodeSeq: ModelToXml[Obj, ?]]: ValidatedRes[Xml] = XmlConverter(model)
   }
 
