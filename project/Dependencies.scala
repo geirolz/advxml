73267f5b14e8e7ec4707091342da183d45c5e629
geirolz <david.geirola@gmail.com>
2020-01-04 12:07:06 +0100

    Update scala xml std library to 2.0.0-M1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     "org.typelevel" %% "cats-core" % "2.1.0" cross CrossVersion.binary,
     "org.scalactic" %% "scalactic" % "3.0.8" cross CrossVersion.binary,
     //XML
-    "org.scala-lang.modules" %% "scala-xml" % "1.2.0" cross CrossVersion.binary,
+    "org.scala-lang.modules" %% "scala-xml" % "2.0.0-M1" cross CrossVersion.binary,
     //TEST
     "org.scalatest" %% "scalatest" % "3.0.8" % Test cross CrossVersion.binary,
     "org.scalatest" %% "scalatest" % "3.0.8" % Test cross CrossVersion.binary,
