0d42707513b01a0487e481b75d60c6f3e6ae7f7e
geirolz <david.geirola@gmail.com>
2020-09-24 11:24:53 +0200

    Replace Traversable with Iterable. Update README.md

--- src/main/scala/advxml/core/utils/TraverserK.scala
+++ src/main/scala/advxml/core/utils/TraverserK.scala
@@ -1,48 +1,48 @@
 package advxml.core.utils
 
 /**
-  * This type describes a traversable behavior of a [[Traversable]] of type [[C]]
+  * This type describes a traversable behavior of a [[Iterable]] of type [[C]]
   * that contains items of [[I]] safety in [[F]].
   *
   * @tparam I Item type.
-  * @tparam C [[Traversable]] of type [[I]].
+  * @tparam C [[Iterable]] of type [[I]].
   * @tparam F Higher-kind type for safe return values.
   */
 //TODO: Common
-trait TraverserK[I, C <: Traversable[I], F[_]] extends Any {
+trait TraverserK[I, C <: Iterable[I], F[_]] extends Any {
 
   /**
     * Find the item at the specified index.
-    * @param c [[Traversable]] instance.
+    * @param c [[Iterable]] instance.
     * @param idx Item index(starts from zero).
     * @return
     */
   def atIndexF(c: C, idx: Int): F[I]
 
   /**
-    * Select the head item of the [[Traversable]].
-    * @param c [[Traversable]] instance.
-    * @return the first element of the [[Traversable]].
+    * Select the head item of the [[Iterable]].
+    * @param c [[Iterable]] instance.
+    * @return the first element of the [[Iterable]].
     */
   def headF(c: C): F[I]
 
   /**
-    * Select the last item of the [[Traversable]]
-    * @param c [[Traversable]] instance.
-    * @return the first element of the [[Traversable]].
+    * Select the last item of the [[Iterable]]
+    * @param c [[Iterable]] instance.
+    * @return the first element of the [[Iterable]].
     */
   def lastF(c: C): F[I]
 
   /**
-    * Select the tail of the [[Traversable]]
-    * @param c [[Traversable]] instance.
-    * @return the [[Traversable]] tail.
+    * Select the tail of the [[Iterable]]
+    * @param c [[Iterable]] instance.
+    * @return the [[Iterable]] tail.
     */
   def tailF(c: C): F[C]
 
   /**
     * Find the first item satisfy the predicate.
-    * @param c [[Traversable]] instance.
+    * @param c [[Iterable]] instance.
     * @param p Predicate used to match item.
     * @return change by the implementation of this type.
     */
