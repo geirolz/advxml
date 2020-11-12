66805aa847e0363bb66d81956df8cf2edc1d47a6
Scala Steward <me@scala-steward.org>
2020-11-13 00:26:40 +0100

    Update discipline-scalatest to 2.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     "org.scala-lang.modules" %% "scala-xml" % "2.0.0-M2" cross CrossVersion.binary,
     //TEST
     "org.scalactic" %% "scalactic" % "3.2.3" % Test cross CrossVersion.binary,
-    "org.typelevel" %% "discipline-scalatest" % "2.0.1" % Test,
+    "org.typelevel" %% "discipline-scalatest" % "2.1.0" % Test,
     "org.typelevel" %% "cats-laws" % "2.1.1" % Test cross CrossVersion.binary,
     "org.scalatest" %% "scalatest" % "3.2.3" % Test cross CrossVersion.binary,
     "org.scalacheck" %% "scalacheck" % "1.14.3" % Test cross CrossVersion.binary
