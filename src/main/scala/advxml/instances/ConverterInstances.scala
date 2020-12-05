b30351bd71f227820c1e787ff5310d402a695f71
geirolz <david.geirola@gmail.com>
2020-12-05 22:02:06 +0100

    Improve ConverterInstances.

--- src/main/scala/advxml/instances/ConverterInstances.scala
+++ src/main/scala/advxml/instances/ConverterInstances.scala
@@ -3,42 +3,19 @@ package advxml.instances
 import advxml.core.{=:!=, ErrorHandler, ExHandler}
 import advxml.core.data._
 import advxml.core.utils.XmlUtils
-import cats.{Applicative, FlatMap, Id, Monad}
+import cats.{Applicative, Functor, Id, Monad}
 
 import scala.util.Try
 import scala.xml.{Elem, Node, Text}
 
-private[instances] trait ConverterInstances {
+private[instances] trait ConverterInstances
+    extends ConverterLowPriorityImplicitsLessGeneric
+    with ConverterLowPriorityImplicitsMostGeneric {
 
-  implicit def identityConverter[F[_]: Applicative, A, B: A =:= *]: Converter[F, A, A] =
-    Converter.id[F, A]
-
-  implicit def convStringToAsConvTextTo[F[_]: FlatMap, T: * =:!= Text](implicit
-    c: StringTo[F, T]
-  ): Converter[F, Text, T] =
-    c.local(_.text)
-
-  implicit def convToStringAsToText[F[_]: FlatMap, T: * =:!= Text](implicit
-    c: ToString[F, T]
-  ): Converter[F, T, Text] =
-    c.map(Text(_))
-
-  implicit def node_to_elem: PureConverter[Node, Elem] =
+  implicit val nodeToElemConverter: PureConverter[Node, Elem] =
     PureConverter.of(XmlUtils.nodeToElem)
 
-  import cats.instances.try_._
-
   // format: off
-  implicit val id_bigInt_to_str     : ToString[Id, BigInt     ] = toString
-  implicit val id_bigDecimal_to_str : ToString[Id, BigDecimal ] = toString
-  implicit val id_byte_to_str       : ToString[Id, Byte       ] = toString
-  implicit val id_short_to_str      : ToString[Id, Short      ] = toString
-  implicit val id_char_to_str       : ToString[Id, Char       ] = toString
-  implicit val id_int_to_str        : ToString[Id, Int        ] = toString
-  implicit val id_long_to_str       : ToString[Id, Long       ] = toString
-  implicit val id_float_to_str      : ToString[Id, Float      ] = toString
-  implicit val id_double_to_str     : ToString[Id, Double     ] = toString
-
   implicit val id_str_to_bigInt     : StringTo[Id, BigInt     ] = liftPure[Try, String, BigInt].map(_.get)
   implicit val id_str_to_bigDecimal : StringTo[Id, BigDecimal ] = liftPure[Try, String, BigDecimal].map(_.get)
   implicit val id_str_to_byte       : StringTo[Id, Byte       ] = liftPure[Try, String, Byte].map(_.get)
@@ -59,15 +36,33 @@ private[instances] trait ConverterInstances {
   implicit def monad_str_to_long      [F[_] : Monad : ExHandler]: StringTo[F, Long      ] = fromString(_.toLong)
   implicit def monad_str_to_float     [F[_] : Monad : ExHandler]: StringTo[F, Float     ] = fromString(_.toFloat)
   implicit def monad_str_to_double    [F[_] : Monad : ExHandler]: StringTo[F, Double    ] = fromString(_.toDouble)
-
   // format: on
-
-  private def toString[I]: ToString[Id, I] =
-    PureConverter.of(v => v.toString)
-
-  private def fromString[F[_]: Monad: ExHandler, O](f: String => O): StringTo[F, O] =
-    Converter.of(s => ErrorHandler.fromTry(Try(f(s))))
 
   private def liftPure[F[_], A, B](implicit c: Converter[F, A, B]): PureConverter[A, F[B]] =
     PureConverter.of(a => c.lift[Id].apply(a))
+
+  private def fromString[F[_]: Monad: ExHandler, O](f: String => O): StringTo[F, O] =
+    Converter.of(s => ErrorHandler.fromTry(Try(f(s))))
+}
+
+private sealed trait ConverterLowPriorityImplicitsLessGeneric {
+
+  implicit def toStringConverter[F[_]: Applicative, T: * =:!= String]: ToString[F, T] =
+    Converter.of(a => Applicative[F].pure(a.toString))
+
+  implicit def convStringToAsConvTextTo[F[_], T: * =:!= Text](implicit
+    c: Converter[F, String, T]
+  ): Converter[F, Text, T] =
+    c.local(_.text)
+
+  implicit def convToStringAsToText[F[_]: Functor, T: * =:!= Text](implicit
+    c: Converter[F, T, String]
+  ): Converter[F, T, Text] =
+    c.map(Text(_))
+}
+
+private sealed trait ConverterLowPriorityImplicitsMostGeneric {
+
+  implicit def identityConverter[F[_]: Applicative, A, B: A =:= *]: Converter[F, A, A] =
+    Converter.id[F, A]
 }
--- src/test/scala/advxml/instances/ConverterInstancesTest.scala
+++ src/test/scala/advxml/instances/ConverterInstancesTest.scala
@@ -5,8 +5,9 @@ import cats.{Id, Monad}
 import org.scalatest.funsuite.AnyFunSuite
 
 import scala.util.Try
-
 import advxml.instances.convert._
+
+import scala.xml.Text
 
 class Common_ConvertersInstancesTest extends AnyFunSuite with ConvertersAssertsUtils {
 
@@ -18,45 +19,77 @@ class Common_ConvertersInstancesTest extends AnyFunSuite with ConvertersAssertsU
   // format: on
 }
 
-class ToText_ConvertersInstancesTest extends AnyFunSuite with ConvertersAssertsUtils {
+class ToString_ConvertersInstancesTest extends AnyFunSuite with ConvertersAssertsUtils {
   // format: off
-  id_bigInt_to_str      test(BigInt(1)          , "1"     )
-  id_bigDecimal_to_str  test(BigDecimal(1.1234) , "1.1234")
-  id_byte_to_str        test(1.toByte           , "1"     )
-  id_short_to_str       test(1.toShort          , "1"     )
-  id_char_to_str        test('A'                , "A"     )
-  id_int_to_str         test(1                  , "1"     )
-  id_long_to_str        test(1L                 , "1"     )
-  id_float_to_str       test(1.0f               , "1.0"   )
-  id_double_to_str      test(1.0d               , "1.0"   )
+  Converter[Id, BigInt     , String].test(BigInt(1)          , "1"     )
+  Converter[Id, BigDecimal , String].test(BigDecimal(1.1234) , "1.1234")
+  Converter[Id, Byte       , String].test(1.toByte           , "1"     )
+  Converter[Id, Short      , String].test(1.toShort          , "1"     )
+  Converter[Id, Char       , String].test('A'                , "A"     )
+  Converter[Id, Int        , String].test(1                  , "1"     )
+  Converter[Id, Long       , String].test(1L                 , "1"     )
+  Converter[Id, Float      , String].test(1.0f               , "1.0"   )
+  Converter[Id, Double     , String].test(1.0d               , "1.0"   )
   // format: on
 }
 
-class FromText_ConvertersInstancesTest extends AnyFunSuite with ConvertersAssertsUtils {
+class FromString_ConvertersInstancesTest extends AnyFunSuite with ConvertersAssertsUtils {
 
   import cats.instances.try_._
 
   // format: off
-  id_str_to_bigInt         test("1"       , BigInt(1)         )
-  id_str_to_bigDecimal     test("1.1234"  , BigDecimal(1.1234))
-  id_str_to_byte           test("1"       , 1.toByte          )
-  id_str_to_short          test("1"       , 1.toShort         )
-  id_str_to_char           test("A"       , 'A'               )
-  id_str_to_int            test("1"       , 1                 )
-  id_str_to_long           test("1"       , 1L                )
-  id_str_to_float          test("1.0"     , 1.0f              )
-  id_str_to_double         test("1.0"     , 1.0d              )
+  //String => T
+  Converter[Id, String, BigInt     ].test("1"       , BigInt(1)         )
+  Converter[Id, String, BigDecimal ].test("1.1234"  , BigDecimal(1.1234))
+  Converter[Id, String, Byte       ].test("1"       , 1.toByte          )
+  Converter[Id, String, Short      ].test("1"       , 1.toShort         )
+  Converter[Id, String, Char       ].test("A"       , 'A'               )
+  Converter[Id, String, Int        ].test("1"       , 1                 )
+  Converter[Id, String, Long       ].test("1"       , 1L                )
+  Converter[Id, String, Float      ].test("1.0"     , 1.0f              )
+  Converter[Id, String, Double     ].test("1.0"     , 1.0d              )
   
-  monad_str_to_bigInt     [Try] test("1"       , BigInt(1)         )
-  monad_str_to_bigDecimal [Try] test("1.1234"  , BigDecimal(1.1234))
-  monad_str_to_byte       [Try] test("1"       , 1.toByte          )
-  monad_str_to_short      [Try] test("1"       , 1.toShort         )
-  monad_str_to_char       [Try] test("A"       , 'A'               )
-  monad_str_to_int        [Try] test("1"       , 1                 )
-  monad_str_to_long       [Try] test("1"       , 1L                )
-  monad_str_to_float      [Try] test("1.0"     , 1.0f              )
-  monad_str_to_double     [Try] test("1.0"     , 1.0d              )
+  //String => T
+  Converter[Try, String, BigInt     ].test("1"       , BigInt(1)         )
+  Converter[Try, String, BigDecimal ].test("1.1234"  , BigDecimal(1.1234))
+  Converter[Try, String, Byte       ].test("1"       , 1.toByte          )
+  Converter[Try, String, Short      ].test("1"       , 1.toShort         )
+  Converter[Try, String, Char       ].test("A"       , 'A'               )
+  Converter[Try, String, Int        ].test("1"       , 1                 )
+  Converter[Try, String, Long       ].test("1"       , 1L                )
+  Converter[Try, String, Float      ].test("1.0"     , 1.0f              )
+  Converter[Try, String, Double     ].test("1.0"     , 1.0d              )
   // format: on
+}
+
+class ConvertersInstancesTestForText extends AnyFunSuite with ConvertersAssertsUtils {
+
+  import cats.instances.try_._
+  import advxml.instances.convert._
+
+  // format: off
+  //Text => T
+  //String => BigDecimal 
+  Converter[Try, Text, BigInt     ].test(Text("1")  , BigInt(1)     )
+  Converter[Try, Text, BigDecimal ].test(Text("1")  , BigDecimal(1) )
+  Converter[Try, Text, Byte       ].test(Text("1")  , 1.toByte      )
+  Converter[Try, Text, Short      ].test(Text("1")  , 1.toShort     )
+  Converter[Try, Text, Char       ].test(Text("A")  , 'A'           )
+  Converter[Try, Text, Int        ].test(Text("1")  , 1             )
+  Converter[Try, Text, Long       ].test(Text("1")  , 1L            )
+  Converter[Try, Text, Float      ].test(Text("1.0"), 1.0f          )
+  Converter[Try, Text, Double     ].test(Text("1.0"), 1.0d          )
+
+  //T => Text
+  Converter[Try, BigInt     , Text].test(BigInt(1)    , Text("1")  )
+  Converter[Try, BigDecimal , Text].test(BigDecimal(1), Text("1")  )
+  Converter[Try, Byte       , Text].test(1.toByte     , Text("1")  )
+  Converter[Try, Short      , Text].test(1.toShort    , Text("1")  )
+  Converter[Try, Char       , Text].test('A'          , Text("A")  )
+  Converter[Try, Int        , Text].test(1            , Text("1")  )
+  Converter[Try, Long       , Text].test(1L           , Text("1")  )
+  Converter[Try, Float      , Text].test(1.0f         , Text("1.0"))
+  Converter[Try, Double     , Text].test(1.0d         , Text("1.0"))
 }
 
 private[instances] sealed trait ConvertersAssertsUtils { $this: AnyFunSuite =>
