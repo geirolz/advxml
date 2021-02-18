8367e89cf1f064196c9983a5dbc0492010e67bfc
Scala Steward <me@scala-steward.org>
2021-02-18 13:16:34 +0100

    Update scalactic to 3.2.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     //XML
     "org.scala-lang.modules" %% "scala-xml" % "2.0.0-M3" cross CrossVersion.binary,
     //TEST
-    "org.scalactic" %% "scalactic" % "3.2.3" % Test cross CrossVersion.binary,
+    "org.scalactic" %% "scalactic" % "3.2.4" % Test cross CrossVersion.binary,
     "org.typelevel" %% "discipline-scalatest" % "2.1.1" % Test,
     "org.typelevel" %% "cats-laws" % "2.3.1" % Test cross CrossVersion.binary,
     "org.scalatest" %% "scalatest" % "3.2.3" % Test cross CrossVersion.binary,
