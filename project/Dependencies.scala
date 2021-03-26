83f2acacf5deca84a5b29e85a11eb0c6b076b9ef
Scala Steward <me@scala-steward.org>
2021-03-26 20:16:01 +0100

    Update scalatest to 3.2.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
     "org.scalactic" %% "scalactic" % "3.2.6" % Test cross CrossVersion.binary,
     "org.typelevel" %% "discipline-scalatest" % "2.1.1" % Test,
     "org.typelevel" %% "cats-laws" % "2.4.2" % Test cross CrossVersion.binary,
-    "org.scalatest" %% "scalatest" % "3.2.3" % Test cross CrossVersion.binary,
+    "org.scalatest" %% "scalatest" % "3.2.6" % Test cross CrossVersion.binary,
     "org.scalacheck" %% "scalacheck" % "1.15.3" % Test cross CrossVersion.binary
   )
 }
