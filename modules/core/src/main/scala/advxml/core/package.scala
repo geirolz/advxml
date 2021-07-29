04b0e93cd4ec87702043c46d5d5917e76ab4ebdb
geirolz <david.geirola@gmail.com>
2021-07-29 18:48:15 +0200

    Rename `AppNelEx` into `AppNelThrow`

--- modules/core/src/main/scala/advxml/core/package.scala
+++ modules/core/src/main/scala/advxml/core/package.scala
@@ -7,6 +7,7 @@ package object core {
   type MonadEx[F[_]] = MonadError[F, Throwable]
 
   type AppEx[F[_]] = ApplicativeError[F, Throwable]
-  type AppNelEx[F[_]] = ApplicativeError[F, ThrowableNel]
+
+  type AppNelThrow[F[_]] = ApplicativeError[F, ThrowableNel]
   type AppEu[F[_]] = ApplicativeError[F, Unit]
 }
