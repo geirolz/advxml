56c87f15d3863f13a8bbaa1948a29ed1d605cf04
Scala Steward <me@scala-steward.org>
2021-11-09 10:23:21 +0100

    Update scala-collection-compat to 2.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,18 +1,18 @@
 import sbt.{CrossVersion, _}
 
-/** Advxml
-  * Created by geirolad on 30/07/2019.
+/** Advxml Created by geirolad on 30/07/2019.
   *
-  * @author geirolad
+  * @author
+  *   geirolad
   */
 object Dependencies {
 
   lazy val common: Seq[ModuleID] = Seq(
-    //SCALA
+    // SCALA
     "org.typelevel" %% "cats-core" % "2.6.1" cross CrossVersion.binary,
-    //XML
+    // XML
     "org.scala-lang.modules" %% "scala-xml" % "2.0.1" cross CrossVersion.binary,
-    //TEST
+    // TEST
     "org.scalactic" %% "scalactic" % "3.2.10" % Test cross CrossVersion.binary,
     "org.typelevel" %% "discipline-scalatest" % "2.1.5" % Test,
     "org.typelevel" %% "cats-laws" % "2.6.1" % Test cross CrossVersion.binary,
@@ -28,7 +28,7 @@ object Dependencies {
   }
 
   lazy val extraDependenciesForScala2_12: Seq[ModuleID] =
-    Seq("org.scala-lang.modules" %% "scala-collection-compat" % "2.5.0")
+    Seq("org.scala-lang.modules" %% "scala-collection-compat" % "2.6.0")
 
   lazy val extraDependenciesForScala2_13: Seq[ModuleID] = Nil
 
