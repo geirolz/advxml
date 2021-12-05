5bf24fd79db8a910805dfbf69a5b78bf0fa1c4e7
geirolz <david.geirola@gmail.com>
2021-12-05 20:38:52 +0100

    Remove Xml alias

--- modules/core/src/main/scala/advxml/advxml.scala
+++ modules/core/src/main/scala/advxml/advxml.scala
@@ -4,10 +4,6 @@ import cats.ApplicativeError
 import scala.xml.NodeSeq
 
 package object advxml {
-
-  type Xml = NodeSeq
-  val Xml = NodeSeq
-
   type ApplicativeNelThrow[F[_]] = ApplicativeError[F, ThrowableNel]
   type ApplicativeEu[F[_]]       = ApplicativeError[F, Unit]
 }
