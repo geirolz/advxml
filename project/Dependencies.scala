70066c7493314d4e8d853c1910cb42421a73d150
Scala Steward <me@scala-steward.org>
2022-05-09 18:24:28 +0200

    Revert commit(s) ebb2997

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,11 +13,11 @@ object Dependencies {
     // XML
     "org.scala-lang.modules" %% "scala-xml" % "2.1.0" cross CrossVersion.binary,
     // TEST
-    "org.scalactic"  %% "scalactic"            % "3.2.12" % Test cross CrossVersion.binary,
-    "org.typelevel"  %% "discipline-scalatest" % "2.1.5"  % Test,
-    "org.typelevel"  %% "cats-laws"            % "2.7.0"  % Test cross CrossVersion.binary,
-    "org.scalatest"  %% "scalatest"            % "3.2.11" % Test cross CrossVersion.binary,
-    "org.scalacheck" %% "scalacheck"           % "1.15.4" % Test cross CrossVersion.binary
+    "org.scalactic" %% "scalactic" % "3.2.11" % Test cross CrossVersion.binary,
+    "org.typelevel" %% "discipline-scalatest" % "2.1.5" % Test,
+    "org.typelevel" %% "cats-laws" % "2.7.0" % Test cross CrossVersion.binary,
+    "org.scalatest" %% "scalatest" % "3.2.11" % Test cross CrossVersion.binary,
+    "org.scalacheck" %% "scalacheck" % "1.15.4" % Test cross CrossVersion.binary
   )
 
   object Plugins {
