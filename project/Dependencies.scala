974adba3d43be85dfe520c5f74dc78811832a251
Scala Steward <scala_steward@virtuslab.com>
2022-07-06 16:11:39 +0000

    Update cats-core, cats-laws to 2.8.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,13 +9,13 @@ object Dependencies {
 
   lazy val common: Seq[ModuleID] = Seq(
     // SCALA
-    "org.typelevel" %% "cats-core" % "2.7.0" cross CrossVersion.binary,
+    "org.typelevel" %% "cats-core" % "2.8.0" cross CrossVersion.binary,
     // XML
     "org.scala-lang.modules" %% "scala-xml" % "2.1.0" cross CrossVersion.binary,
     // TEST
     "org.scalactic"  %% "scalactic"            % "3.2.12" % Test cross CrossVersion.binary,
     "org.typelevel"  %% "discipline-scalatest" % "2.1.5"  % Test,
-    "org.typelevel"  %% "cats-laws"            % "2.7.0"  % Test cross CrossVersion.binary,
+    "org.typelevel"  %% "cats-laws"            % "2.8.0"  % Test cross CrossVersion.binary,
     "org.scalatest"  %% "scalatest"            % "3.2.12" % Test cross CrossVersion.binary,
     "org.scalacheck" %% "scalacheck"           % "1.15.4" % Test cross CrossVersion.binary
   )
